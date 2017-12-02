6eee5bdda88acaf1d2c79fc82d43fa35b29b056c
Ruohan <ruohanzhan@gmail.com>
2017-12-01 19:17:09 -0800

    update baseline

--- EigenPortfolio.py
+++ EigenPortfolio.py
@@ -1,38 +1,31 @@
 import sys, os, csv
 import numpy as np
-from utils import dataUtil as du
+from utils import dataUtil
 
-def main():
-    
-    # get data
-    dateSelected, stockPrice = du.getData()
-    
-    # get time for baseline estimation  
-    # [0,10],[0,11],...,[0,len(dateSelected)]
-    Time = xrange(10,len(dateSelected)+1) 
-    Date = [dateSelected[i-2][0] for i in Time]
-    
+def baseline(stockPrice, Time):
+    # Time: time period index for baseline estimation  
+    # estimate Return[i] = Return for Date period [Time[i],Time[i]+1]
     estimateReturn = np.zeros(len(Time)) 
     
     # total asset
     M = np.zeros(len(Time)+1) 
-    M[0] = 28348.5754100817
+    M[0] = 1 
     
     # parameters for transaction cost
-    c = np.zeros(len(stockPrice[-1])+1) + 0.001
+    c = np.zeros(len(stockPrice[-1])+1) + 0.0001
     
     # calculate return including transaction cost
     for i in xrange(len(Time)):
-        # estimateReturn[i] is return for [Time[i]-1, Time[i]]
-        estimateReturn[i], eigenportfolio = calculateReturn(stockPrice[0:Time[i]]) 
+        # estimateReturn[i] is return for [Time[i], Time[i]+1]
+        estimateReturn[i], eigenportfolio = calculateReturn(stockPrice[0:Time[i]+1]) 
         if i == 0:
-            estimateReturn[i] -= 0.002
+            estimateReturn[i] -= c[0]+c[1]
             M[i+1] = M[i]*(1 + estimateReturn[i])
             beforePt = eigenportfolio
         else:
             flag = 0
             for j in xrange(len(stockPrice[-1])):
-                tmp = eigenportfolio[j]-beforePt[j]*M[i-1]/M[i]*stockPrice[Time[i]-1][j]/stockPrice[Time[i]-2][j]
+                tmp = eigenportfolio[j]-beforePt[j]*M[i-1]/M[i]*stockPrice[Time[i]][j]/stockPrice[Time[i]-1][j]
                 estimateReturn[i] = estimateReturn[i] - c[j+1] * np.abs(tmp)
                 if tmp != 0:
                     flag = 1
@@ -41,7 +34,7 @@ def main():
             M[i+1] = M[i]*(1 + estimateReturn[i])
             beforePt = eigenportfolio
     
-    return estimateReturn, Date, M
+    return estimateReturn
     
 
 # stockReturn: return for each time period
@@ -79,14 +72,59 @@ def getPeriodReturn(stockPrice):
 
     
 def baselineReturn(stockReturn, mu):
-    cov = du.getCovarianceMatrix(stockReturn[:-1], mu)
-    eigenportfolio = du.getLargestEigenvector(cov)
+    cov = getCovarianceMatrix(stockReturn[:-1], mu)
+    eigenportfolio = getLargestEigenvector(cov)
     portReturn = np.dot(eigenportfolio, stockReturn[-1])
     return portReturn, eigenportfolio
     
 
+# exponential weighted average mean and covariance matrix
+# stocknum: num of stockPrice
+# mu: hyperparameter for weighted exponential mean 
+# T: number of time period
+def getCovarianceMatrix(stockReturn, mu):
+    stocknum = len(stockReturn[0])
+    returnSum = np.zeros(stocknum)
+    T = len(stockReturn)
+    denumerator = 0
+    
+    # calculate weighted exponential mean
+    for i in xrange(T):
+        returnSum = returnSum + stockReturn[i] * np.exp(-mu * (T-i))
+        denumerator += np.exp(-mu * (T-i))
+    meanReturn = returnSum / denumerator
+
+    # calculate weighted exponential covariance matrix
+    cov = np.zeros((stocknum, stocknum))
+    for i in xrange(T):
+        normalizedReturn = (stockReturn[i] - meanReturn)
+        cov = cov + \
+        np.dot(np.transpose(normalizedReturn),normalizedReturn) * np.exp(-mu * (T-i))
+    
+    cov  = cov / denumerator
+    return cov
+        
+    
+# calculate largest eval-eigenvector 
+def getLargestEigenvector(cov):
+    D, S = np.linalg.eigh(cov)
+    eigenportfolio = S[:,-1] / np.sum(S[:,-1]) 
+    return eigenportfolio
 
 
-if __name__=='__main__':
-    estimateReturn, Date, M = main()
+"""
+# get data (date, stockPrice)
+dateSelected, stockPrice = dataUtil.getData()
+    
+# get time for baseline estimation  
+# [0,10],[0,11],...,[0,len(dateSelected)]
+Time = xrange(10+(len(dateSelected)-10)/2+1,len(dateSelected)) 
+
+# Date for estimated return period
+Date = [(dateSelected[i-2][0],dateSelected[i-1][0]) for i in Time]
+
+# estimated period return
+estimateReturn = baseline(stockPrice, Time)
+"""
+
 
