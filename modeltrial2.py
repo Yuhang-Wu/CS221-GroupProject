d304ff71f2cafa9f0c284e5f92980fa90069a9a0
Ruohan <ruohanzhan@gmail.com>
2017-12-04 08:35:41 -0800

    update train-validation-test in modeltrail2.py

--- modeltrial2.py
+++ modeltrial2.py
@@ -6,17 +6,16 @@ from models import dummyModel as dm
 from models import cnnModel as cm
 from models import rnnModel as rm
 from models import rnnModel2 as rm2
+from utils import plotAndEval
 from utils import readin, yfReader, dataUtil as du, getPlots as gp
 import EigenPortfolio as ep
 import logging
 import os
 from utils import plotAndEval as pe
-import oracle as oc
 
 DATA_PATH = 'data/sp10/'
 DATA_PATH_ALL = 'data/sp150'
 D = 10
-N = 10
 c = 0.0001
 epochs = 20
 transCostParams = {
@@ -25,57 +24,100 @@ transCostParams = {
 }
 baselineTransCostParams = np.zeros(D + 1) + c
 
-resultsDirectory = 'results/allresults/' + du.getCurrentTimestamp() 
-os.mkdir(resultsDirectory)
+# resultsDirectory = 'results/allresults/' + du.getCurrentTimestamp()
+# os.mkdir(resultsDirectory)
 
 # now call logger.info to log
-logger = du.setupLogger(resultsDirectory)
+# logger = du.setupLogger(resultsDirectory)
+
+
+## specify testing data
+# get data for testing
+dateSelected, stockPrice = du.getData(DATA_PATH, 'week')
+# get testing data index
+TestTimeIndex = range(len(dateSelected)/10*9, len(dateSelected))
+# get testing time period Date for estimating return (startDate, endDate)
+TestDate = [(dateSelected[i-1][0],dateSelected[i][0]) for i in TestTimeIndex]
+
+# generate xticks for plotting
+xticks = du.date2xtick(dateSelected[i] for i in TestTimeIndex)
+    
+# epochs and tolerance for training
+epochs = 500
+tol = 1e-7
 
 def main():
-    print(du.getCurrentTimestamp())
+    N = [1]
+    kernelSize = [1]
+    
+    ModelGrowthRates = []
+    maxReturn = -1000
+    maxN = -1
+    maxkernelSize = -1
+    for Ni in N:
+        for kernelSizei in kernelSize:
+            print(du.getCurrentTimestamp())
+            resultsDirectory = 'results/allresults/' + du.getCurrentTimestamp()
+            os.mkdir(resultsDirectory)
 
-    trainAndTestTrial()
-    #rnnModelTrainingTrial()
+            # now call logger.info to log
+            logger = du.setupLogger(resultsDirectory)
+            growthRates, totalGR = TrainAndValidTrial(Ni, logger, kernelSizei)
+            if maxReturn < totalGR:
+                maxReturn = totalGR
+                maxN = Ni
+                maxkernelSize = kernelSizei
+
+            # ModelGrowthRates.append(growthRates)
+
+    print ('maxN is {}, maxkernelSize is {}'.format(maxN, maxkernelSize))
+
+    """
+    baselineGrowthRates = 1.0 + ep.baseline(stockPrice, TestTimeIndex, baselineTransCostParams)
+    baselineTotalGR = du.prod(baselineGrowthRates)
+    HyperparameterPlot = plotAndEval.plotEval(TestDate, 2016)
+    HyperparameterPlot.addReturn(baselineGrowthRates, 'baseline')
+    for i in xrange(len(N)):
+        HyperparameterPlot.addReturn(ModelGrowthRates[i], 'cnnModel, N = ' + str(N[i]))
+    HyperparameterPlot.generatePlot()
+    HyperparameterPlot.eval()
+    """
 
 
-def trainAndTestTrial():
-    ## specify testing data
-    # get data for testing
-    dateSelected, stockPrice = du.getData(DATA_PATH, 'week')
-    # get testing data index
-    TestTimeIndex = range(len(dateSelected)/2, len(dateSelected))
-    # get testing time period Date for estimating return (startDate, endDate)
-    TestDate = [(dateSelected[i-1][0],dateSelected[i][0]) for i in TestTimeIndex]
-
+def TrainAndValidTrial(N, logger, kernelSize):
+    
     # get testing data index
     TestIndex = [i-N-1 for i in TestTimeIndex]
+    # get validation data index
+    ValidationIndex = range(TestIndex[0]/9*8, TestIndex[0])
     # get training data index
-    TrainIndex = range(TestIndex[0]) 
+    TrainIndex = range(TestIndex[0]/9*8)
 
     returnTensor, prevReturnMatrix, nextReturnMatrix = du.getInputs(stockPrice, N)
-    print(returnTensor.shape)
- 
+
+
     ## get training data
     returnTensor_Train = np.array([returnTensor[_] for _ in TrainIndex])
     prevReturnMatrix_Train = np.array([prevReturnMatrix[_] for _ in TrainIndex])
     nextReturnMatrix_Train = np.array([nextReturnMatrix[_] for _ in TrainIndex])
-    print(returnTensor_Train.shape)
+
+    ## get validation data
+    returnTensor_Valid = np.array([returnTensor[_] for _ in ValidationIndex])
+    prevReturnMatrix_Valid = np.array([prevReturnMatrix[_] for _ in ValidationIndex])
+    nextReturnMatrix_Valid = np.array([nextReturnMatrix[_] for _ in ValidationIndex])
+
     ## get testing data
     returnTensor_Test = np.array([returnTensor[_] for _ in TestIndex])
     prevReturnMatrix_Test = np.array([prevReturnMatrix[_] for _ in TestIndex])
-    nextReturnMatrix_Test = np.array([nextReturnMatrix[_] for _ in TestIndex])    
-    
-    # generate xticks for plotting
-    xticks = du.date2xtick(dateSelected[i] for i in TestTimeIndex)
+    nextReturnMatrix_Test = np.array([nextReturnMatrix[_] for _ in TestIndex])
 
-    # epochs and tolerance for training 
-    epochs = 500
+
+    # epochs and tolerance for training
     logger.info('total epochs: '+str(epochs))
-    tol = 1e-7
     logger.info('tolerance: '+str(tol))
     
     # define model
-    curModel = cm.CnnModel(D, N, transCostParams)
+    curModel = cm.CnnModel(D, N, transCostParams, kernelSize)
     L = 1
 
     model_info = curModel.get_model_info()
@@ -102,23 +144,19 @@ def trainAndTestTrial():
             else:
                 preTotalGR = totalGR
 
-        ## test model
-        allActions, growthRates = mu.test1epoch(returnTensor_Test, prevReturnMatrix_Test, nextReturnMatrix_Test, curModel, sess)
+        ## valid model
+        allActions, growthRates = mu.test1epoch(returnTensor_Valid, prevReturnMatrix_Valid, nextReturnMatrix_Valid, curModel, sess)
+        totalGR = du.prod(growthRates)
+        logger.info('model total growth rate in validation data: '+ str(totalGR))
+        
         print(allActions[0])
 
-        baselineGrowthRates = 1.0 + ep.baseline(stockPrice, TestTimeIndex, baselineTransCostParams)
-        oracleGrowthRates = oc.solveOracle(stockPrice, 10000, transCostParams['c'], transCostParams['c0'])
-        growthRates = growthRates[-len(baselineGrowthRates):]
-        totalGR = du.prod(growthRates)
-        baselineTotalGR = du.prod(baselineGrowthRates)
-        plotE = pe.plotEval(TestDate, 2016)
-        plotE.addReturn(baselineGrowthRates, 'Baseline')
-        plotE.addReturn(growthRates, 'CNN')
-        plotE.addReturn(oracleGrowthRates, 'Oracle')
-        plotE.generatePlot()
-        logger.info('model total growth rate in testing data: '+ str(totalGR))
-        logger.info('baseline total growth rate: '+str(baselineTotalGR))
-        logger.info('')
+
+        # growthRates = growthRates[-len(baselineGrowthRates):]
+
+    return growthRates, totalGR
+
 
 if __name__=='__main__':
     main()
+
